0a483f248ff41860a2e2c2c6fc313e8b64a1e053
Mikael Ivarsson <mikael.ivarsson4@gmail.com>
2023-06-14 21:56:31 +0200

    exists endpoint

--- routes/play.py
+++ routes/play.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, Response
 import json
-
+from os import path
 play = APIRouter(prefix="/play")
 
 
@@ -11,3 +11,11 @@ async def get_puzzle(p: str):
         data = json.load(test)
 
     return JSONResponse(data)
+
+@play.get("/puzzle/exists")
+async def get_puzzle_exists(p: str):
+    puzzle_id = p.casefold()
+    exists = path.exists(f"routes/{puzzle_id}.json")
+    if not exists:
+        return Response(status_code=404)
+    return Response(status_code=204)
